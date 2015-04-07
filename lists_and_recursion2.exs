ab361c139d526e33780823c8968cd59f961df45f
Przemek <Przemek@Przemeks-MacBook.local>
2015-04-07 20:16:30 +0200

    Find max element in a list

---
+++ lists_and_recursion2.exs
@@ -0,0 +1,22 @@
+defmodule ExampleList do
+  @initial_max 0
+  def max(list) do
+    _max(list, @initial_max)
+  end
+
+  defp _max([], current_max) do
+    current_max
+  end
+
+  defp _max([head | tail], current_max) when head > current_max do
+    _max(tail, head)
+  end
+
+  defp _max([_head | tail], current_max) do
+    _max(tail, current_max)  
+  end
+end
+
+
+IO.puts ExampleList.max([1, 2, 3])
+IO.puts ExampleList.max([99, 2, 3])
